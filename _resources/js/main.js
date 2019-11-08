18a0737b164b01a99ca6af1598fd9b59f0665dae
El-Chapo0133 <levequeloris66@gmail.com>
2019-11-08 22:02:59 +0100

    initial code made

---
+++ _resources/js/main.js
@@ -0,0 +1,48 @@
+/** CONSTANTS */
+const MAINDIV = document.getElementById("mainDiv")
+const INDEXMAXX = 10
+const INDEXMAXY = 3
+
+/** VARIABLES */
+var _document = document
+
+/** CLASSES */
+class Initializer {
+    constructor(p_jQuery) {
+        this.jQuery = p_jQuery
+    }
+    initialize() {
+        for (var indexX = 0; indexX < INDEXMAXX; indexX++) {
+            for (var indexY = 0; indexY < INDEXMAXY; indexY++) {
+                this.addDiv(indexX, indexY)
+            }
+        }
+        this.setMaindiv(INDEXMAXX * 120 + 40, INDEXMAXY * 120 + 40)
+    }
+    addDiv(x,y) {
+        MAINDIV.innerHTML += '<div id="' + x + "-" + y + '" class="itemGrid"></div>'
+        var div = _document.getElementById(x + "-" + y)
+        this.jQuery.moveElement(div,x * 120,y * 120)
+    }
+    setMaindiv(x,y) {
+        MAINDIV.style.width = x + "px"
+        MAINDIV.style.height = y + "px"
+    }
+}
+class JQuery {
+    moveElement(element, x, y) {
+        element.style.left = String(x) + "px"
+        element.style.top = String(y) + "px"
+    }
+}
+
+/** INSTANTIATION */
+let jQuery = new JQuery()
+let initializer = new Initializer(jQuery)
+
+/** FUNCTION */
+
+/** EVENTS */
+
+/** OTHER */
+initializer.initialize()
